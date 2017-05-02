1234473b23c0544a9616f1486a8b2ed43b0cd22a
David Leonardi <david.leonardi@gmail.com>
2017-05-02 16:50:22 +0200

    adding unit test structure

---
+++ js/array/circularCounter/CircularCounter.spec.js
@@ -0,0 +1,10 @@
+const assert = require('assert');
+// const CircularCounter = require('./CircularCounter');
+
+describe('array', function() {
+    describe('circularCounter', function() {
+        it('should be able to start a test', function() {
+            assert.equal(true, true);
+        });
+    });
+});
